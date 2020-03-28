c0b3d3b3443b7661d35ce5437d06c54e2d972eab
André Monello <atmonello@outlook.com>
2020-03-28 01:05:19 -0300

    Implementing color and quantity filter in Card controller

--- src/controllers/CardController.ts
+++ src/controllers/CardController.ts
@@ -1,17 +1,32 @@
 import { Request, Response } from "express";
 import Card from "../schemas/Card";
-import Logger from "../services/Logger";
+import Logger from "../utils/Logger";
+import {shuffleArray} from "../utils/Functions"
 
 export default {
   async index(req: Request, res: Response) {
-    const { color } = req.query;
+    const { color, quantity } = req.query;
 
-    let cards;
+    let cards:any[] = [];
+
+    const totalCards = await Card.find();
 
     if (color) {
-      cards = await Card.find({ color });
+      const colorCards = totalCards.filter(card => card.color === color);
+
+      if (quantity) {
+        const cardsQty = Number(quantity);
+        const filter = shuffleArray(colorCards).slice(0, cardsQty);
+        cards = [...cards, filter];
+      } else {
+        cards = [...cards, colorCards];
+      }
+    } else if (quantity) {
+      const cardsQty = Number(quantity);
+      const filter = shuffleArray(totalCards).slice(0, cardsQty);
+      cards = [...cards, filter];
     } else {
-      cards = await Card.find();
+      cards = [...cards, totalCards];
     }
 
     if (cards.length <= 0) {
